9ae13857df049dc5533f5f5a54b2cfb9ec9006bb
Vinay Kumar <vinayjangra.dev@gmail.com>
2025-10-13 23:15:28 +0530

    Setup backend structure

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,21 @@
 import express from 'express'
 const app = express();
 
-app.get("/", (res, req) =>   {
-        res.send("Hello world")
+import {nanoid} from 'nanoid'
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.post("/api/create", (req, res) =>   {
+    const url = req.body
+    console.log(url);
+    res.send("hello world")
 })
 
 app.listen(3000, () => {
-    console.log("Server is running on port")
+    console.log("Server is running on port 3000")
 })
+
+//GET -> redirection
+//POST -> create short URL
---
+++ Backend/src/config/db.js
@@ -0,0 +1,17 @@
+import pkg from 'pg'
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+const {Pool} = pkg;
+
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+})
+
+pool.on("connect", () => {
+    console.log("Connected to postgresSQL")
+});
+
+export default pool;
